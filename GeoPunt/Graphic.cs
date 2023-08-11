6eb632bf585b69dac697c6acf4f24c50555663aa
heliasesri <helias.breneol@esribelux.com>
2023-08-11 11:24:20 +0200

    Graphic class

---
+++ GeoPunt/Graphic.cs
@@ -0,0 +1,21 @@
+﻿using ArcGIS.Core.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoPunt
+{
+    public class Graphic
+    {
+        public Dictionary<string, object> Attributes { get; set; }
+        public Geometry Geometry { get; set; }
+
+        public Graphic(Dictionary<string, object> attributes, Geometry geometry)
+        {
+            Attributes = attributes;
+            Geometry = geometry;
+        }
+    }
+}
