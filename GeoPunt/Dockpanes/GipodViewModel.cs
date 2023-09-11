8f636b73744a59378bc15ada3a16d81974d81afc
heliasesri <helias.breneol@esribelux.com>
2023-09-11 18:13:40 +0200

    Basic GIPOD dockpane

---
+++ GeoPunt/Dockpanes/GipodViewModel.cs
@@ -0,0 +1,62 @@
+﻿using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Layouts;
+using ArcGIS.Desktop.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GeoPunt.Dockpanes
+{
+    internal class GipodViewModel : DockPane
+    {
+        private const string _dockPaneID = "GeoPunt_Dockpanes_Gipod";
+
+        protected GipodViewModel() { }
+
+        /// <summary>
+        /// Show the DockPane.
+        /// </summary>
+        internal static void Show()
+        {
+            DockPane pane = FrameworkApplication.DockPaneManager.Find(_dockPaneID);
+            if (pane == null)
+                return;
+
+            pane.Activate();
+        }
+
+        /// <summary>
+        /// Text shown near the top of the DockPane.
+        /// </summary>
+        private string _heading = "Bevraag GIPOD";
+        public string Heading
+        {
+            get => _heading;
+            set => SetProperty(ref _heading, value);
+        }
+    }
+
+    /// <summary>
+    /// Button implementation to show the DockPane.
+    /// </summary>
+    internal class Gipod_ShowButton : Button
+    {
+        protected override void OnClick()
+        {
+            GipodViewModel.Show();
+        }
+    }
+}
