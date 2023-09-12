9cf10ee0bd7e54dc29b24853df47c5e0a33bae33
heliasesri <helias.breneol@esribelux.com>
2023-09-12 15:48:34 +0200

    Base test part

---
+++ GeopuntTests1/ArcGISTestClassAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeopuntTests1
+{
+    public class ArcGISTestClass : Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute
+    {
+        public ArcGISTestClass(string productId = "ArcGISPro") : base()
+        {
+            // Install domain wide assembly resolver
+            TestResolver.Install(productId);
+        }
+    }
+}
---
+++ GeopuntTests1/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace GeopuntTests1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
