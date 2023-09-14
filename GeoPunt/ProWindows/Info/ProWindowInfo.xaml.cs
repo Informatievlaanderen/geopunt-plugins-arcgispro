8895b7634cada9ad62760a426c7506fd986678c0
Vexcel OPC <OPC@eewwem.int>
2023-09-14 10:51:41 +0200

    functional hyperlinks + removed texts in red + folder organization

--- GeoPunt/ProWindows/Info/ProWindowInfo.xaml.cs
+++ GeoPunt/ProWindows/Info/ProWindowInfo.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,14 @@ namespace GeoPunt
         public ProWindowInfo()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Open Hyperlink in default browser when clicked.
+        /// </summary>
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
         }
     }
 }
