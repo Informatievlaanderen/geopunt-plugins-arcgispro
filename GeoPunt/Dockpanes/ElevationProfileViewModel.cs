76f2f7ce7f3c03d550e082b095909bc933f3632d
heliasesri <helias.breneol@esribelux.com>
2023-09-22 14:00:10 +0200

    Scottplot first version

--- GeoPunt/Dockpanes/ElevationProfileViewModel.cs
+++ GeoPunt/Dockpanes/ElevationProfileViewModel.cs
@@ -1,25 +1,17 @@
 ﻿using ArcGIS.Core.CIM;
-using ArcGIS.Core.Data;
 using ArcGIS.Core.Geometry;
-using ArcGIS.Desktop.Catalog;
-using ArcGIS.Desktop.Core;
-using ArcGIS.Desktop.Editing;
-using ArcGIS.Desktop.Extensions;
 using ArcGIS.Desktop.Framework;
 using ArcGIS.Desktop.Framework.Contracts;
 using ArcGIS.Desktop.Framework.Dialogs;
 using ArcGIS.Desktop.Framework.Threading.Tasks;
-using ArcGIS.Desktop.Layouts;
 using ArcGIS.Desktop.Mapping;
-using GeoPunt.DrawTools;
+using ScottPlot;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// using System.Windows.Forms;
 using System.Windows.Input;
+using static System.Net.WebRequestMethods;
 
 namespace GeoPunt.Dockpanes
 {
@@ -32,6 +24,15 @@ namespace GeoPunt.Dockpanes
         protected ElevationProfileViewModel()
         {
             Module1.ElevationProfileViewModel = this;
+
+
+            double[] dataX = new double[] { 1, 2, 3, 4, 5 };
+            double[] dataY = new double[] { 1, 4, 9, 16, 25 };
+
+            PlotControl = new WpfPlot();
+            PlotControl.Plot.AddScatter(dataX, dataY);
+            PlotControl.Refresh();
+            PlotControl.Plot.Title("Hoogte profiel");
 
         }
 
@@ -61,23 +62,65 @@ namespace GeoPunt.Dockpanes
             {
                 SetProperty(ref _profileLine, value);
 
-                if(_profileLine != null)
+                if (_profileLine != null)
                 {
 
                     QueuedTask.Run(() =>
                     {
                         ClearDisposable();
-
-                        //Set symbolology, create and add element to layout
-                        // CIMStroke outlineMulti = SymbolFactory.Instance.ConstructStroke(ColorFactory.Instance.GreenRGB, 2.0, SimpleLineStyle.Solid);
-                        CIMLineSymbol lineSymbol = SymbolFactory.Instance.ConstructLineSymbol(ColorFactory.Instance.GreenRGB, 2);
-                        _profileLineDisposable = MapView.Active.AddOverlay(_profileLine, lineSymbol.MakeSymbolReference());
+                        DisplayDisposable(_profileLine);
                     });
+
+
+                    UpdatePlot(_profileLine);
+
 
 
                 }
             }
         }
+
+       
+
+        private void DisplayDisposable(Polyline profileLine)
+        {  
+            //Set symbolology, create and add element to layout
+           // CIMStroke outlineMulti = SymbolFactory.Instance.ConstructStroke(ColorFactory.Instance.GreenRGB, 2.0, SimpleLineStyle.Solid);
+            CIMLineSymbol lineSymbol = SymbolFactory.Instance.ConstructLineSymbol(ColorFactory.Instance.GreenRGB, 2);
+            _profileLineDisposable = MapView.Active.AddOverlay(profileLine, lineSymbol.MakeSymbolReference());
+        }
+
+        private void UpdatePlot(Polyline profileLine)
+        {
+            ReadOnlyPointCollection lineVertices = profileLine.Points;
+            double baseNumber = GeometryEngine.Instance.GeodesicLength(profileLine, LinearUnit.Meters) / lineVertices.Count;
+            double[] dataX = new double[lineVertices.Count];
+            // Populate the array with multiples
+            for (int i = 0; i < lineVertices.Count; i++)
+            {
+                dataX[i] = (i) * baseNumber;
+            }
+
+            double[] dataY = (from lineVertex in lineVertices select lineVertex.Z).ToArray();
+
+
+            PlotControl = new WpfPlot();
+            PlotControl.Plot.AddScatter(dataX, dataY);
+            PlotControl.Refresh();
+            PlotControl.Plot.Title("Hoogte profiel");
+        }
+
+        private WpfPlot _plotControl;
+        public WpfPlot PlotControl
+        {
+            get { return _plotControl; }
+            set
+            {
+                SetProperty(ref _plotControl, value);
+            }
+        }
+
+
 
         public ICommand CmdAddLayer
         {
@@ -86,21 +129,26 @@ namespace GeoPunt.Dockpanes
                 return new RelayCommand(async () =>
                 {
 
-                    if(MapView.Active == null)
+                    if (MapView.Active == null)
                     {
                         MessageBox.Show("No map view active.");
                         return;
                     }
 
                     // Create a connection to the WMS server
+                    // todo change with this // https://geo.api.vlaanderen.be/DHMV/wcs // DHMV2DTM
                     var serverConnection = new CIMInternetServerConnection { URL = "https://geo.api.vlaanderen.be/DHMV/wms" };
+
+                    // var serverConnection = new CIMInternetServerConnection { URL = "https://geo.api.vlaanderen.be/el-dtm/wcs" };
+
+                    // var test = new CIMWCSServiceConnection { ServerConnection = serverConnection, CoverageName = "EL.GridCoverage.DTM", };
                     var connection = new CIMWMSServiceConnection { ServerConnection = serverConnection };
 
                     // Add a new layer to the map
                     var layerParams = new LayerCreationParams(connection);
 
-                    if(hoogteWMS != null)
-                    { 
+                    if (hoogteWMS != null)
+                    {
                         MapView.Active.Map.RemoveLayer(hoogteWMS);
                         hoogteWMS = null;
                     }
@@ -122,8 +170,9 @@ namespace GeoPunt.Dockpanes
         }
 
 
-        private void ClearDisposable() {
-            if(_profileLineDisposable != null)
+        private void ClearDisposable()
+        {
+            if (_profileLineDisposable != null)
                 _profileLineDisposable.Dispose();
 
         }
